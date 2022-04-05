7f149db3764ce84ae140ec9047b0637b8083605d
NeoCambion <jmcarpenter42@gmail.com>
2022-04-05 20:32:14 +0100

    Scripted push of local changes

--- Assets/Scripts/Environment/TextureTiling.cs
+++ Assets/Scripts/Environment/TextureTiling.cs
@@ -5,32 +5,42 @@ using UnityEngine.UI;
 
 public class TextureTiling : MonoBehaviour
 {
-	#region [ PARAMETERS ]
-	
+    #region [ PARAMETERS ]
+
+    private Vector2 faceSize = new Vector2();
+    private Material surfaceMat;
+    private float matScaleFactor = 0.2f;
+
 	#endregion
 
     /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
     void Awake()
     {
-        
-    }
-
-    void Start()
-    {
-        
-    }
-	
-    void Update()
-    {
-        
-    }
-
-    void FixedUpdate()
-    {
-        
+        surfaceMat = gameObject.GetComponent<MeshRenderer>().materials[0];
+        GetFaceSize();
+        surfaceMat.mainTextureScale = faceSize * matScaleFactor;
     }
 
     /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 	
+    private void GetFaceSize()
+    {
+        Vector3 scale = gameObject.transform.localScale;
+        if (scale.x < scale.y && scale.x < scale.z)
+        {
+            faceSize[0] = scale.z;
+            faceSize[1] = scale.y;
+        }
+        else if (scale.y < scale.z)
+        {
+            faceSize[0] = scale.x;
+            faceSize[1] = scale.z;
+        }
+        else
+        {
+            faceSize[0] = scale.x;
+            faceSize[1] = scale.y;
+        }
+    }
 }
