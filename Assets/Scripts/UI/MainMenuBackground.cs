06f2b282d9a68c669ed8d1ed63ab5e4250121a87
NeoCambion <jmcarpenter42@gmail.com>
2022-04-15 19:05:14 +0100

    Scripted push of local changes

--- Assets/Scripts/UI/MainMenuBackground.cs
+++ Assets/Scripts/UI/MainMenuBackground.cs
@@ -6,30 +6,35 @@ using UnityEditor;
 
 public class MainMenuBackground : MonoBehaviour
 {
-	#region [ PARAMETERS ]
-	
+    #region [ PARAMETERS ]
+
+    private float aspectRatio;
+    private float aspect1080p = 1920.0f / 1080.0f;
+
+    private RectTransform rect;
+
+    private RectTransform canvas;
+    private CanvasScaler canvasScaler;
+
 	#endregion
 
     /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
     void Awake()
     {
-        
-    }
+        rect = gameObject.GetComponent<RectTransform>();
+/*
+        aspectRatio = Screen.width / Screen.height;
+        float heightRatio = 1080.0f / Screen.height;
+        float h = 1080.0f * heightRatio;
+        float w = (1920.0f / 1080.0f) * h;
 
-    void Start()
-    {
-        
-    }
-	
-    void Update()
-    {
-        
-    }
+        if (true)
+        {
 
-    void FixedUpdate()
-    {
-        
+        }
+
+        rect.sizeDelta = new Vector2(w, h);*/
     }
 
     /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
