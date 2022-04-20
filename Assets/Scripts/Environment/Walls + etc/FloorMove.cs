2409b0afa13a7647a961eb2fc9a59e8386d67a49
NeoCambion <jmcarpenter42@gmail.com>
2022-04-20 12:22:51 +0100

    Scripted push of local changes

--- Assets/Scripts/Environment/Walls + etc/FloorMove.cs
+++ Assets/Scripts/Environment/Walls + etc/FloorMove.cs
@@ -50,7 +50,7 @@ public class FloorMove : CoreFunctionality
         // Make sure to make it "listName.Count - 1", though, else you may hit a fringe case where
         // it tries to access a value at an index outside the list range
         // hold = Random.Range(0, 3);
-        hold = RandomInt(0, floors.Count);
+        hold = RandomInt(0, floors.Count - 1);
         floors[hold].transform.position = new Vector3(playzone.x, playzone.y, playzone.z);
         }
     }
